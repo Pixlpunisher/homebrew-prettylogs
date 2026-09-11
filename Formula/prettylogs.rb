0cbff0f91e8afad6a173e966e80c5f43f4c23833
Pixlpunisher <tobias.contardo@ne.se>
2026-09-11 20:01:31 +0200

    Fix revision SHA (use commit, not tag object)

--- Formula/prettylogs.rb
+++ Formula/prettylogs.rb
@@ -3,7 +3,7 @@ class Prettylogs < Formula
   homepage "https://github.com/Pixlpunisher/prettylogs"
   url "https://github.com/Pixlpunisher/prettylogs.git",
       tag:      "v0.1.0",
-      revision: "ea9efb45dbbe11dbacff7bdda886715a6f3aff83"
+      revision: "e1b8fbcf148efe4b9adc6a6513627f12d9d20fb7"
   license "MIT"
   head "https://github.com/Pixlpunisher/prettylogs.git", branch: "main"
 
